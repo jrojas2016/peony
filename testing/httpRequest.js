bedcf0a111af2f8cf62385414b1e604b7be3fc48
jrojas2016 <jrojas2016@gmail.com>
2016-08-29 18:17:02 -0700

    httpRequest on App side test code

--- testing/httpRequest.js
+++ testing/httpRequest.js
@@ -1,36 +1,45 @@
-var http = require('http');
+var EventEmitter = require("events").EventEmitter;
+var str = new EventEmitter();
+var res = '';
 
-//The url we want is: 'http://peony-curie.herokuapp.com/getPushNotifications'
-var options = {
-	host: 'peony-curie.herokuapp.com',
-	path: '/getPushNotifications',
-	method: 'GET'
-};
+function getNotifications(pHost, pPath) {
+	// var http = require('http');
+	var http = new http();
+	//The url we want is: 'http://peony-curie.herokuapp.com/getPushNotifications'
+	var options = {
+		host: pHost,
+		path: pPath,
+		method: 'GET'
+	};
 
-callback = function(response) {
-	var str = '';
-	console.log(response.statusCode);
-	response.setEncoding('utf8');
-	//another chunk of data has been recieved, so append it to `str`
-	response.on('data', function (chunk) {
-		console.log('New data arriving!');
-		str += chunk;
-	});
+	callback = function(response) {
+		// var str = '';
+		console.log(response.statusCode);
+		response.setEncoding('utf8');
+		console.log(response.data)
+		//another chunk of data has been recieved, so append it to `str`
+		response.on('data', function (chunk) {
+			console.log('New data arriving!');
+			str.data += chunk;
+		});
 
-	//the whole response has been recieved, so we just print it out here
-	response.on('end', function () {
-		//Send to Genuino
-		console.log(str);
-		console.log('End of request reached!');
-	});
+		//the whole response has been recieved, so we just print it out here
+		response.on('end', function () {
+			//Send to Genuino
+			console.log(str.data);
+			console.log('End of request reached!');
+			str.emit('update')
+			// return str;
+		});
+	}
 
+	var req = http.request(options, callback);
+	req.end();
 }
 
-var req = http.request(options, callback);
-
-req.on('error', function(e) {
-	console.log(e.message);
+var notif = getNotifications('peony-curie.herokuapp.com', '/getPushNotifications')
+str.on('update', function () {
+    console.log(str.data); // HOORAY! THIS WORKS!
+    res = str.data;
 });
-
-req.end();
 
